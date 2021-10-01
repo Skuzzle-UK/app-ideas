8427d7e7aaea251fb6c357a8f0dcf76c292d1978
Nick Bailey <nick@skuzzle.com>
2021-10-01 23:03:37 +0100

    Small bug if null input

---
+++ Projects/1-Beginner/Bin2Dec/Bin2Dec/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Binary;
+using Bin = Binary.Binary;
+
+namespace B2D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //get binary number
+            try
+            {
+                Console.WriteLine("Input a binary number:");
+                Console.Write(">");
+                Bin binary = new(Console.ReadLine());
+
+                //converts to decimal
+                decimal decoutput = binary.ToDecimal();
+                Console.WriteLine();
+                Console.Write("Converted to decimal: ");
+                Console.WriteLine(decoutput);
+
+                //converts back to binary
+                Bin binoutput = Bin.ToBinary(decoutput);
+                Console.Write("Converted back to binary: ");
+                Console.WriteLine(binoutput.Value);
+            }
+            catch { Console.WriteLine("Input was not a binary number"); }
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit . . .");
+            Console.ReadLine();
+        }
+    }
+}
